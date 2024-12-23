ba829e6727c08d84cf8fa433efc171e0d1aab700
Matt <matthew.lewis@emergentmethod.com>
2024-12-23 10:06:46 -0600

    Removed unused environment variable.

--- src/lib/server/api/craft-api.ts
+++ src/lib/server/api/craft-api.ts
@@ -1,10 +1,11 @@
-import { Craft_API, CRAFT_TOKEN } from '$env/static/private';
+import { CRAFT_TOKEN } from '$env/static/private';
+import { PUBLIC_CRAFT_API } from '$env/static/public';
 
 export async function fetchCraftApi(requestBody: any) {
 	const apiHeaders = new Headers();
 	apiHeaders.append('content-type', 'application/json');
 
-	const apiRequest = new Request(Craft_API, {
+	const apiRequest = new Request(PUBLIC_CRAFT_API, {
 		method: 'POST',
 		headers: {
 			'content-type': 'application/json',
